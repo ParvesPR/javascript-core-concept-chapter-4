787874f0f83025ff82b8b1c2e0d1f04d44f1bb1f
ParvesPR <parves2585@gmail.com>
2024-02-26 23:19:56 +0800

    string literal and constructor

---
+++ string.js
@@ -0,0 +1,6 @@
+// String declaration method
+var str = 'this is string'; /* String literal */
+var str2 = 10;
+var str3 = String(str2); /* String constructor */
+var str4 = str3.toString(); /* String constructor */
+console.log(str, str4);
