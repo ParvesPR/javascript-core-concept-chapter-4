f1c54fded5b433276a62a580bf6b0d0652027fda
ParvesPR <parves2585@gmail.com>
2024-02-27 00:07:33 +0800

    escape notation in javascript

--- string.js
+++ string.js
@@ -4,3 +4,18 @@ var str2 = 10;
 var str3 = String(str2); /* String constructor */
 var str4 = str3.toString(); /* String constructor */
 console.log(str, str4);
+
+/* Escape Notation */
+var str = 'This is a \'string\''; /* Print single quote */
+var str2 = 'This is a \nstring'; /* A new line */
+var str3 = 'This is a \tstring'; /* Horizontal Tabulator */
+console.log(str3);
+
+/*The most commonly used of the many escape notations 
+\\ – Backslash.
+\' – Single quote.
+\" – Double quote.
+\n – Newline.
+\r – Carriage return.
+\t – Tab.
+*/
